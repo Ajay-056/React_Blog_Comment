479ee7edb47b8bfc60ac4ce45869a0b0f7a1652d
AK <balaajay19@gmail.com>
2020-09-14 16:52:24 +0530

    Reused Comment component along with faker js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
+
+import Comment from "./components/Comment";
 
 const App = () => {
-  return <div>Hi there!</div>;
+  return (
+    <div className="ui container comments">
+      <Comment
+        image={faker.image.avatar()}
+        authorName={faker.internet.userName()}
+        postedTime={new Date().toDateString()}
+        commentMsg={faker.lorem.sentence()}
+      />
+
+      <Comment
+        image={faker.image.avatar()}
+        authorName={faker.internet.userName()}
+        postedTime={new Date().toDateString()}
+        commentMsg={faker.lorem.sentence()}
+      />
+
+      <Comment
+        image={faker.image.avatar()}
+        authorName={faker.internet.userName()}
+        postedTime={new Date().toDateString()}
+        commentMsg={faker.lorem.sentence()}
+      />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
