1d3ca20a62496c3cf9ac29d8e542875b8a08e871
AK <balaajay19@gmail.com>
2020-09-14 12:55:12 +0530

    Renamed File

---
+++ src/First_React_App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  const buttonText = "Click Me!";
+  //   return React.createElement("div", null, "Hello, there!");
+  // OR return <div>Hi there!</div>;
+  return (
+    <div>
+      {/* // class is a javascript keyword so we have to use className instead of class */}
+      <label className="label" for="name">
+        Enter name:
+      </label>
+      <input type="text" id="name" />
+      {/* //The custom styling changes in JSX */}
+      <button style={{ backgroundColor: "blue", color: "#000" }}>
+        {/* // we can use JS variables inside JSX */}
+        {buttonText}
+      </button>
+    </div>
+  );
+};
+
+// ReactDOM.render(<App />, document.querySelector("#root"));
